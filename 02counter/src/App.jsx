890f2aca6c2d86612ccc960bda639ddd70800dd9
Sushant Kumar <sushantkumar9378@gmail.com>
2026-03-21 08:07:00 +0530

    button will be disable when it reaches the limit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,29 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+function App() { 
 
 let [counter, setCounter] = useState(15) //using useState hook to create a state variable 'counter' and a function 'setCounter' to update it. Initial value is 15.
    //let counter = 15
 
 const addValue = () => {
-   
-  counter = counter + 1
-  setCounter(counter)
+  if(counter < 20){
+  setCounter(counter +1 ) //update the counter state variable by incrementing its value by 1
 }
-
+}
 const removeValue = () => {
-  console.log('clicked', counter );   
-  counter = counter - 1
-  setCounter(counter)
+  if(counter > 0){
+  setCounter(counter -1 ) //update the counter state variable by decrementing its value by 1
+}
 }
   return (
     <>
       <h1>sushant and react</h1>
       <h2>counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add Value{counter}</button>
+      <button onClick={addValue} disabled={counter === 20}>Add Value{counter}</button>
       <br />
-      <button onClick={removeValue}>
+      <button onClick={removeValue} disabled={counter === 0}> 
         remove Value{counter}</button>
       <p>footer: {counter} </p>
     </>
